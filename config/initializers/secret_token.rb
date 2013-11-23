5069540a4c2030617528ecec538d39aa5fb4128b
Marcus Smith <mail4marcus@comcast.net>
2013-11-23 14:44:11 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GroupRide::Application.config.secret_token = '4f6b888082b22ae2e73abe4f0ea906beead16a912c2226176fd492a317970c292f896df5a5ac09915ffc837b3ad539c38fa068f2a492216bf045c06fd2b0d1ee'
