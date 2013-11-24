cecb32e79ada1b259474444ab7930c3bf39c895f
Marcus Smith <mail4marcus@comcast.net>
2013-11-23 22:44:05 -0800

    Passing all the test, going to improve routes.rb from get to match

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ GroupRide::Application.routes.draw do
   get "static_pages/home"
   get "static_pages/contact"
   get "static_pages/help"
+  get "static_pages/about"
 
   resources :events
 
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -29,7 +29,7 @@ end
   end
   
   
-  describe "Contact" do
+  describe "Contact page" do
     it "should have the h1 'Contact'" do
       visit '/static_pages/contact'
       page.should have_selector('h1', :text => 'Contact')
@@ -41,5 +41,16 @@ end
                     :text => "Ruby on Rails Tutorial Sample App | Contact")
     end
   end
+  describe "About page" do
+    it "should have the h1 'About'" do
+      visit '/static_pages/about'
+      page.should have_selector('h1', :text => 'About Us')
+    end
 
+    it "should have the title 'About Us'" do
+      visit '/static_pages/about'
+      page.should have_selector('title',
+                    :text => "Ruby on Rails Tutorial Sample App | About Us")
+    end
+  end
 end
