4ffb9dedb23a3a34a388f63cd797d5d652fe91df
Marcus Smith <mail4marcus@comcast.net>
2013-11-23 16:39:50 -0800

    Added Title Helper function

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
-end
+  # Returns the full title on a per-page basis.
+  def full_title(page_title)
+    base_title = "Group Ride DEFAULT"
+    if page_title.empty?
+      base_title
+    else
+      "#{base_title} | #{page_title}"
+    end
+end end
